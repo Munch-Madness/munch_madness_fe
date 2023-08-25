1377f3918f68e18d5cc4672c3c77044febbcbe63
Seth Burns <seth@sethburns.com>
2023-08-25 12:55:28 -0700

    Add Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
   const [restaurants, setRestaurants] = useState(['Applebees', 'Taco Star', 'Red Robin', 'Red Lobster', 'McDonalds', 'Taco Bell', 'iHop', 'Cheba Hut'])
   // 'Applebees', 'Taco Star', 'Red Robin', 'Red Lobster', 'McDonalds', 'Taco Bell', 'iHop', 'Cheba Hut', 'Applebees', 'Taco Star', 'Red Robin', 'Red Lobster', 'McDonalds', 'Taco Bell', 'iHop', 'Cheba Hut'
   return (
-    <div className="App">
+    <div className="flex flex-col justify-center bg-gray-400">
+      <h1 className='text-center text-4xl'>Munch Madness</h1>
       <Bracket restaurants={restaurants}/>
     </div>
   );
