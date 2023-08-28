438811b641696ed59393ecbc7b5c64f5e5995c43
Seth Burns <seth@sethburns.com>
2023-08-28 13:14:27 -0700

    Start Cypress Testing

---
+++ cypress/e2e/bracket-spec.cy.ts
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.get('button').first().click()
+  })
+  it('should show a bracket of 16, then 8, then 4, then 2, then winner, and allow choosing winner for every round', () => {
+    cy.get('p').first().should('have.text', 'Omakase')
+  })
+})
