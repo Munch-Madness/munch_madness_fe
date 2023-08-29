99fb68556b045f787fd172f805b0f7ff8ce84108
Isaiah Richardson <capcinematic@gmail.com>
2023-08-29 17:20:26 -0600

    add error handling and proper URL for getting data

--- src/components/apiCalls.ts
+++ src/components/apiCalls.ts
@@ -1,8 +1,12 @@
 function getData(){
-  fetch('https://munch-madness-be-8b56c3719f5f.herokuapp.com/api/restaurants')
-  .then(response => response.json())
+  fetch("https://munch-madness-be-8b56c3719f5f.herokuapp.com/api/v1/places/search?query=chicken")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Problem with Network");
+    }
+    return response.json();
+  })
   .then(data => console.log(data))
   .catch(error => console.error(error));
 }
-getData()
 export default getData
