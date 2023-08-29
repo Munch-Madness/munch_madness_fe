018846aa1a064058a6d9f9d12f81f62ef38c4d6f
Isaiah Richardson <capcinematic@gmail.com>
2023-08-29 16:47:27 -0600

    updated app.tsx to match main after merger error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-// import './App.css';
 import React, { useState } from 'react';
 import Bracket from './components/Bracket/Bracket'
 import { BracketChoice } from './components/BracketChoice/BracketChoice';
+import SelectMenu from './components/SelectMenu/SelectMenu';
 
 function App() {
   const [bracketSize, setBracketSize] = useState(0)
-
-
+  const [allRestaurants, setAllRestaurants] = useState([
+  'Omakase',
+  'Freebirds',
+  'Wendys',
+  'Dennys',
+  'McDonalds',
+  'Taco Bell',
+  'Burger King',
+  'Chipotle',
+  'Applebees',
+  'Taco Star',
+  'Red Robin',
+  'Red Lobster',
+  'Ruths Chris',
+  'Burgerville',
+  'iHop',
+  'Cheba Hut'
+])
+  const [restaurants, setRestaurants] = useState([]);
+  const [selected, setSelected] = useState(false)
+console.log(restaurants)
   
   return (
     <div className="App flex flex-col justify-center items-center bg-background">
