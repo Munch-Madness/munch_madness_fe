13c853e0c5077e44070d2fcad88147db09067dab
Seth Burns <seth@sethburns.com>
2023-08-31 15:40:55 -0700

    Add serviceWorker solution for Cypress

--- cypress/e2e/bracket-spec.cy.ts
+++ cypress/e2e/bracket-spec.cy.ts
@@ -1,11 +1,25 @@
+async function unreg() {
+  if (!window.navigator || !navigator.serviceWorker) {
+    return null;
+  }
+  const registrations = await navigator.serviceWorker.getRegistrations();
+  return Promise.all(registrations.map(registration => registration.unregister()));
+}
+
+function cleanUp() {
+  cy.clearAllCookies();
+}
+
 describe('template spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
     cy.get('button').first().click()
   })
   it('should show reastaurants for user to choose from', () => {
+    unreg()
     cy.get('button').first().should('have.text', 'Go Back').click()
     cy.get('button').first().should('have.text', '16').click()
     cy.get('.list').children('ul')
+    cleanUp()
   })
 })
